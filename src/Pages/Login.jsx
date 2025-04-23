941f5b1a908af7f6be0d4c512babbfb1247a2a2f
usman <usmanbalogun044@gmail.com>
2025-04-23 16:54:47 -0700

    ra

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -39,7 +39,7 @@ const Login = () => {
     try {
       setLoading(true);
 
-      const url = process.env.REACT_APP_API_URL + '/signin';
+      const url = `${process.env.REACT_APP_API_URL}/signin`;
 
       // "https://toplike.up.railway.app/api/signin";
       const options = {
