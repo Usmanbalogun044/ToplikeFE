6200a13799f65733bb78b705c128fcbb516e4598
usman <usmanbalogun044@gmail.com>
2025-09-15 10:55:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
+//server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
